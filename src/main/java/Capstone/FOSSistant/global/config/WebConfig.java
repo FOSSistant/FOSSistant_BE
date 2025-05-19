dbe0bd2a25dce67ce27d18dc186673ab73ab0add
hanjk0816 <hanjk0816@naver.com>
2025-05-19 23:04:11 +0900

    fix: CORS설정 추가

--- src/main/java/Capstone/FOSSistant/global/config/WebConfig.java
+++ src/main/java/Capstone/FOSSistant/global/config/WebConfig.java
@@ -17,7 +17,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000",
+                        "https://fossistant.duckdns.org")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 허용할 HTTP 메소드
                 .allowedHeaders("*") // 허용할 헤더
                 .allowCredentials(true); // 자격 증명 허용
