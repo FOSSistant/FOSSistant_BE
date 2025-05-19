1c7af715989a4f53bf9456c13114099c577cb5ba
hanjk0816 <hanjk0816@naver.com>
2025-05-20 02:05:08 +0900

    fix: Security에 Cors설정 추가

--- src/main/java/Capstone/FOSSistant/global/config/SecurityConfig.java
+++ src/main/java/Capstone/FOSSistant/global/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package Capstone.FOSSistant.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -26,6 +27,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(Customizer.withDefaults())
                 .httpBasic(httpBasic -> httpBasic.disable())
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
